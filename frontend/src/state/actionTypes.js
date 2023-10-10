6ccd2bef68cbbe7a4262bb5afaafc3ab415ef0ea
Arit Chanda <aritchanda1234@gmail.com>
2023-10-10 00:44:42 -0700

    Added actionTypes and updated store

--- frontend/src/state/actionTypes.js
+++ frontend/src/state/actionTypes.js
@@ -1,4 +1,24 @@
+// PRODUCT
 export const AddProductToDB = "PRODUCT.ADDPRODUCTTODB";
 export const AddProductToStore = "PRODUCT.ADDPRODUCTTOSTORE";
 export const GetAllProductsFromDB = "PRODUCT.GETALLPRODUCTSFROMDB";
 export const AddProductsToStore = "PRODUCT.ADDPRODUCTSTOSTORE";
+
+// API STATE
+export const APIisLoading = "API.ISLOADING";
+export const APIisSuccess = "API.ISSUCCESS";
+export const APIisError = "API.ISERROR";
+
+// CART
+export const AddCartItemToStore = "CART.ADDCARTITEMTOSTORE";
+export const ClearCartStoreItems = "CART.CLEARCARTSTOREITEMS";
+export const UpdateCartItem = "CART.UPDATECARTITEM";
+export const RemoveItemFromCart = "CART.REMOVEITEMFROMCART";
+export const SaveCartToDB = "CART.SAVETODB";
+export const GetCartFromDB = "CART.GETFROMDB";
+
+//USER
+export const AddUserToStore = "USER.ADDUSERTOSTORE";
+export const AddUserToDB = "USER.ADDUSERTODB";
+export const LoginUser = "USER.LOGIN";
+export const LogoutUser = "USER.LOGOUT";
--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -5,10 +5,16 @@ import thunk from "redux-thunk";
 
 import productReducer from "./Product/ProductReducer";
 import productsReducer from "./Products/ProductsReducer";
+import apiReducer from "./APIState/APIStateReducer";
+import cartReducer from "./Cart/CartReducer";
+import userReducer from "./User/UserReducer";
 
 const rootReducer = combineReducers({
   productReducer,
   productsReducer,
+  apiReducer,
+  cartReducer,
+  userReducer,
 });
 
 export default configureStore(
