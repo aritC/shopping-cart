c910b3ebdcad8a42d8a61ed592c45f1274a7bb20
Arit Chanda <aritchanda1234@gmail.com>
2023-10-11 09:30:58 -0700

    Fixed routes bug for admin

--- frontend/src/app/app.js
+++ frontend/src/app/app.js
@@ -1,4 +1,5 @@
 import React, { Suspense } from "react";
+import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import "./app.css";
@@ -7,21 +8,30 @@ import NavBar from "./components/Navbar/NavBar";
 import navRoutes from "./routes/routes";
 
 const App = () => {
+  const user = useSelector((store) => store.userReducer.User);
   return (
     <Router>
       <NavBar navItems={navRoutes} />
       <Suspense fallback={<h1 className="text-danger">Loading....</h1>}>
         <Routes>
-          {navRoutes.map((navItem, idx) => {
-            const timestamp = Date.now();
-            return (
-              <Route
-                path={navItem.url}
-                key={idx + timestamp}
-                element={navItem.component}
-              />
-            );
-          })}
+          {navRoutes
+            .filter((navRoute) => {
+              if (navRoute.isAdminRoute && user.userName === "admin") {
+                return navRoute;
+              } else if (!navRoute.isAdminRoute) {
+                return navRoute;
+              }
+            })
+            .map((navItem, idx) => {
+              const timestamp = Date.now();
+              return (
+                <Route
+                  path={navItem.url}
+                  key={idx + timestamp}
+                  element={navItem.component}
+                />
+              );
+            })}
           <Route path="/*" element={<NotFound />} />
         </Routes>
       </Suspense>
